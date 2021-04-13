b59ad7eec5a110540435aaef7fbc44469b504a30
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-04-13 15:28:54 +0200

    renames request name. Altered logo logic. Removed console logs.

--- js/pixabay.js
+++ js/pixabay.js
@@ -1,26 +1,27 @@
+let pixabayRequest = "https://pixabay.com/api/";
+pixabayRequest = pixabayRequest + "?q=" + town;
+pixabayRequest = pixabayRequest + "&image_type=all"; // Get all image types. Set to photo if results get wierd
+pixabayRequest = pixabayRequest + "&safesearch=true"; //  Filter for only images suitable for all ages 
+pixabayRequest = pixabayRequest + "&lang=da"; // 	Language code of the language to be searched in. If removed defaults to en
+pixabayRequest = pixabayRequest + "&lang=da"; 
+pixabayRequest = pixabayRequest + "&page=1"; // Result page  (also is used in relation to per_page)
+pixabayRequest = pixabayRequest + "&per_page=5"; // Limit results (also is used in relation to page)
+pixabayRequest = pixabayRequest + "&key=" + pixabay_key;
+//console.log(pixabayRequest);
 
-let request = "https://pixabay.com/api/";
-request = request + "?q=" + town;
-request = request + "&image_type=all"; // Get all image types. Set to photo if results get wierd
-request = request + "&safesearch=true"; //  Filter for only images suitable for all ages 
-request = request + "&lang=da"; // 	Language code of the language to be searched in. If removed defaults to en
-request = request + "&lang=da"; 
-request = request + "&page=1"; // Result page  (also is used in relation to per_page)
-request = request + "&per_page=5"; // Limit results (also is used in relation to page)
-request = request + "&key=" + pixabay_key;
-//console.log(request);
-//display on the console the full API request to make sure you did everything right
 let pixabayImages = document.getElementById("pixabay-images")
-console.log(request)
 
-fetch(request)
+fetch(pixabayRequest)
 	.then((response) => {
 		return response.json()
 	})
 	.then((data) => {
-	// Work with JSON data here
+		if (document.contains(document.getElementById("pixabayLogo"))) {
+			// Remove logo on load - forces the logo to be the last image
+            document.getElementById("pixabayLogo").remove();
+		}
 		pixabayImages.classList.remove("hidden");
-		console.log(data.hits)
+		//console.log(data.hits)
 		for (var i = 0; i < data.hits.length; i++){
 			//console.log(data.hits[i])
 			var obj = data.hits[i];
@@ -31,6 +32,7 @@ fetch(request)
 			pixabayImages.appendChild(pixImage);
 		}
 		var pixLogoLink = document.createElement("a");
+		pixLogoLink.setAttribute("id", "pixabayLogo");
 		var pixLogo = document.createElement("img");
 		pixLogo.setAttribute("src", "img/pixabay_logo_square.svg");
 		pixLogo.setAttribute("alt", "Pixabay logo");
@@ -38,7 +40,6 @@ fetch(request)
 		pixLogoLink.setAttribute("href", "https://pixabay.com/");
 		pixLogoLink.setAttribute("target", "_blank");
 		pixLogoLink.setAttribute("title", "Gå til Pixabay");
-		
 		pixabayImages.appendChild(pixLogoLink);
 	})
 	.catch((err) => {

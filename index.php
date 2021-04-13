6c1340b5634c64de377d695ecb1ac5e804478851
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-04-13 11:31:40 +0200

    Added wip.

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- META DESCRIPTIONS -->
+    <title>Byinfo - Info for turister om byer, lokalitet, historie og rejse</title>
+    <meta name="description" content="[by] info for turister. Lokalitet, historie og rejse" />
+
+    <!-- META DATA -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--<meta http-equiv="X-UA-Compatible" content="IE=edge" />-->
+
+    <!-- STYLLING & ICONS (FAV ICON) -->
+	<link type="text/CSS" rel="stylesheet" href="css/byinfo.css?v=1.4"/>
+    <!--<link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png" />
+    <meta name="theme-color" content="#379a45"><!-- 
+	- Set theme color - Affects color of some browser interfaces -- >-->
+	
+	<!-- VIEWPORT -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Enables responsive design. -->
+	
+	<!-- CHARSET -->
+	<meta charset="utf-8" />
+    
+	<!-- ROBOTS -->
+    <meta name="robots" content="noindex" /><!-- Block indexing of site -->
+</head>
+<body>
+<div class="wrapper">
+	<ul id="movie_list"></ul>
+</div>
+<!-- JAVASCRIPT FILES -->
+<script type="text/javascript" src="js/keys.js"></script>
+<script type="text/javascript" src="js/omdbapi.js"></script>
+</body>
